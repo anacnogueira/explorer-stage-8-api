3df7fc5e31dd15ef38147f010f23398570276fc7
Ana Claudia <anacnogueira@gmail.com>
2025-04-16 15:20:38 -0300

    Query params

--- src/server.js
+++ src/server.js
@@ -7,6 +7,11 @@ app.get("/message/:user/:id", (request, response) => {
   response.send(`User: ${user}, Message id: ${id}`);
 });
 
+app.get("/users", (request, response) => {
+  const { page, limit } = request.query;
+  response.send(`Page: ${page}, Show: ${limit} registers`);
+});
+
 const PORT = 3000;
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
