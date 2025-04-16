9264957aea13524b4e2d32cb831eb60ef700b2fd
Ana Claudia <anacnogueira@gmail.com>
2025-04-16 10:45:51 -0300

    GET method

--- src/server.js
+++ src/server.js
@@ -2,6 +2,10 @@ import express from "express";
 
 const app = express();
 
+app.get("/", (request, response) => {
+  response.send("Hello, World!");
+});
+
 const PORT = 3000;
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
