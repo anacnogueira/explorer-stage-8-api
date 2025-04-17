1e8730f835d2dfdc531f1ded5fd6aaa784fd8127
Ana Claudia <anacnogueira@gmail.com>
2025-04-17 16:10:14 -0300

    Using middlewares

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -2,7 +2,17 @@ import { Router } from "express";
 import { UsersController } from "../controllers/UsersController.js";
 
 export const userRoutes = Router();
+
+function myMiddleware(request, response, next) {
+  console.log("you passed the middleware");
+  if (!request.body.isAdmin) {
+    return response.json({ message: "user unauthorized" });
+  }
+  next();
+}
+
 const userController = new UsersController();
+userRoutes.use(myMiddleware);
 
 userRoutes.get("/", (request, response) => {
   const { page, limit } = request.query;
