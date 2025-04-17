b88b8a06b15ebb64bd111f520a4fb31442bbeb86
Ana Claudia <anacnogueira@gmail.com>
2025-04-17 10:07:49 -0300

    Body Params andjson response

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 
 const app = express();
+app.use(express.json());
 
 app.get("/message/:user/:id", (request, response) => {
   const { user, id } = request.params;
@@ -10,6 +11,11 @@ app.get("/message/:user/:id", (request, response) => {
 app.get("/users", (request, response) => {
   const { page, limit } = request.query;
   response.send(`Page: ${page}, Show: ${limit} registers`);
+});
+
+app.post("/users", (request, response) => {
+  const { name, email, password } = request.body;
+  response.json({ name, email, password });
 });
 
 const PORT = 3000;
