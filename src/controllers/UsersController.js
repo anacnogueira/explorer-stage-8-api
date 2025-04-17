4ffe9ec2c0499e04bb7aef5eada937da9f79a6b1
Ana Claudia <anacnogueira@gmail.com>
2025-04-17 11:41:02 -0300

    Creating UsersController

---
+++ src/controllers/UsersController.js
@@ -0,0 +1,9 @@
+export class UsersController {
+  /**
+   * index - GET to list many records.
+   * show - GET to display a specific record.
+   * create - POST to create a record.
+   * update - PUT to update a record.
+   * delete - DELETE to remove a record.
+   */
+}
