e1acde1347fe63b6b8393e6d5e93bd3af31ae81a
Ana Claudia <anacnogueira@gmail.com>
2025-04-23 11:26:38 -0300

    Hashing password and correcting exception of existing user

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,3 +1,5 @@
+import { hash } from "bcryptjs";
+import { AppError } from "../utils/AppError.js";
 import { sqlConnection } from "../database/sqlite/index.js";
 export class UsersController {
   async create(request, response) {
@@ -5,17 +7,18 @@ export class UsersController {
 
     const database = await sqlConnection();
     const checkUserExists = await database.get(
-      "SELECT * FROM users WHERE email - (?)",
+      "SELECT * FROM users WHERE email = (?)",
       [email]
     );
 
     if (checkUserExists) {
-      throw new Exception("This email is already in use");
+      throw new AppError("This email is already in use");
     }
 
+    const hashedPassword = await hash(password, 8);
     await database.run(
       "INSERT INTO users (name, email, password) VALUES(?, ?, ?)",
-      [name, email, password]
+      [name, email, hashedPassword]
     );
 
     return response.status(201).json({});
