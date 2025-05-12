9df91a0a552d9d728cd9ee93cc661e6bcc9eb9f5
Ana Claudia <anacnogueira@gmail.com>
2025-05-12 20:27:01 -0300

    Validate password

--- src/controllers/SessionsController.js
+++ src/controllers/SessionsController.js
@@ -1,14 +1,23 @@
+import { compare } from "bcryptjs";
 import { connection } from "../database/knex/index.js";
 import { AppError } from "../utils/AppError.js";
 export class SessionsController {
   async create(request, response) {
+    const invalidMessage = "Email e/or password invalid.";
     const { email, password } = request.body;
 
     const user = await connection("users").where({ email }).first();
 
     if (!user) {
-      throw new AppError("Email e/or password invalid.", 401);
+      throw new AppError(invalidMessage, 401);
     }
+
+    const passwordMatch = await compare(password, user.password);
+
+    if (!passwordMatch) {
+      throw new AppError(invalidMessage, 401);
+    }
+
     return response.json(user);
   }
 }
