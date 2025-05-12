4d2edc09d2c94c228a47f68420ab3ef44991c1cb
Ana Claudia <anacnogueira@gmail.com>
2025-05-12 20:20:32 -0300

    Validate registered user

--- src/controllers/SessionsController.js
+++ src/controllers/SessionsController.js
@@ -1,6 +1,14 @@
+import { connection } from "../database/knex/index.js";
+import { AppError } from "../utils/AppError.js";
 export class SessionsController {
   async create(request, response) {
     const { email, password } = request.body;
-    return response.json({ email, password });
+
+    const user = await connection("users").where({ email }).first();
+
+    if (!user) {
+      throw new AppError("Email e/or password invalid.", 401);
+    }
+    return response.json(user);
   }
 }
