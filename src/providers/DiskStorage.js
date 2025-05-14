32676d0ea8880244e833b32f9d4d69ce725324f0
Ana Claudia <anacnogueira@gmail.com>
2025-05-14 10:05:57 -0300

    Save and delete file methods

---
+++ src/providers/DiskStorage.js
@@ -0,0 +1,26 @@
+import fs from "fs";
+import path from "path";
+import uploadConfig from "../configs/upload";
+import { diskStorage } from "multer";
+
+export class DiskStorage {
+  async saveFile(file) {
+    await fs.promises.rename(
+      path.resolve(uploadConfig.TMP_FOLDER, file),
+      path.resolve(uploadConfig.UPLOAD_FOLDER, file)
+    );
+
+    return file;
+  }
+
+  async deleteFile(file) {
+    const filePath = path.resolve(uploadConfig.UPLOAD_FOLDER, file);
+    try {
+      await fs.promises.stat(file);
+    } catch (error) {
+      return;
+    }
+
+    await fs.promises.unlink(filePath);
+  }
+}
