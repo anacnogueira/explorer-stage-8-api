2f0e57555be6b47ae6310700a4120e19505af0c2
Ana Claudia <anacnogueira@gmail.com>
2025-05-20 11:26:00 -0300

    Group tags by name

--- src/controllers/TagsController.js
+++ src/controllers/TagsController.js
@@ -4,7 +4,7 @@ export class TagsController {
   async index(request, response) {
     const user_id = request.user.id;
 
-    const tags = await connection("tags").where({ user_id });
+    const tags = await connection("tags").where({ user_id }).groupBy("name");
 
     return response.json(tags);
   }
